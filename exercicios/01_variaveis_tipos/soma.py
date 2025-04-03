666fbb4fd8836a4af21ad0797cdc792c96d326d6
Victor <victormoreiracontact@gmail.com>
2025-04-03 20:54:01 -0300

    pastas + exercicio de soma.

---
+++ exercicios/01_variaveis_tipos/soma.py
@@ -0,0 +1,20 @@
+"""
+Exercício: Soma de dois números
+Nível: Iniciante
+Objetivo: Declarar duas variáveis A e B e printar o resultado na tela.
+"""
+a = 10
+b = 5
+soma = a + b
+print(f"SOMA = {soma}")
+
+"""
+Exercício: Soma de dois números
+Nível: Iniciante
+Objetivo: Pedir dois valores para o usuário e realizar a soma entre eles.
+"""
+
+a = int(input("Digite o valor de A: "))
+b = int(input("Digite o valor de B: "))
+soma = a + b
+print(f"SOMA = {soma}")
